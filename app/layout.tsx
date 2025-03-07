8f25721ea8eb21c07cabfb473cbdc38f531a5d47
Jayshree Anandakumar <ping.jayshree@gmail.com>
2025-03-07 05:49:39 +0530

    Removed dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,6 @@ export const metadata = {
 export const viewport: Viewport = {
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
 };
 
