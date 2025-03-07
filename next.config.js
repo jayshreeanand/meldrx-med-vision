560513a919df85e422dd3cb211a93de1ca455ca3
Jayshree Anandakumar <ping.jayshree@gmail.com>
2025-03-07 07:23:50 +0530

    Build config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  ignoreBuildErrors: true,
 }
 
 module.exports = nextConfig
